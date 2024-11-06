3c1ac80b936377a0c1aecb1139597234d88a3409
Alexandr Chazov <chazovby@gmail.com>
2024-11-06 20:40:35 +0300

    feat: create modules, controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { ConfigModule } from "@nestjs/config";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { AlbumModule } from "./modules/album/album.module";
+import { ArtistModule } from "./modules/artist/artist.module";
+import { FavoriteModule } from "./modules/favorite/favorite.module";
+import { TrackModule } from "./modules/track/track.module";
+import { UserModule } from "./modules/user/user.module";
 
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [
+		ConfigModule.forRoot(),
+		UserModule,
+		TrackModule,
+		ArtistModule,
+		AlbumModule,
+		FavoriteModule,
+	],
 	controllers: [AppController],
 	providers: [AppService],
 })
