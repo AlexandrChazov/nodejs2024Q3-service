8e8a8c4cabc5aeb3c2696e223cb3161689033a72
Alexandr Chazov <chazovby@gmail.com>
2024-11-14 05:38:40 +0300

    feat: save Users in PostgreSQL table

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,41 +3,48 @@ import {
 	Injectable,
 	NotFoundException,
 } from "@nestjs/common";
-import { randomUUID } from "node:crypto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
-import { database } from "../../database";
 import { User } from "../../types";
 import { CreateUserDto, UpdatePasswordDto } from "./dto";
+import { UserEntity } from "./user.entity";
 
 @Injectable()
 export class UserService {
-	async getAll() {
-		return database.users;
+	constructor(
+		@InjectRepository(UserEntity)
+		private userRepository: Repository<UserEntity>,
+	) {}
+
+	async getAll(): Promise<UserEntity[]> {
+		return await this.userRepository.find();
 	}
 
-	async getById(id: string) {
-		const user = database.users.find((user) => user.id === id);
+	async getById(id: string): Promise<UserEntity> {
+		const user = await this.userRepository.findOne({ where: { id } });
 		if (!user) {
 			throw new NotFoundException("User not found");
 		}
 		return { ...user, password: undefined };
 	}
 
-	async create(dto: CreateUserDto): Promise<User> {
-		const newUser: User = {
-			id: randomUUID(),
-			login: dto.login,
-			password: dto.password,
-			version: 1,
-			createdAt: new Date().getTime(),
-			updatedAt: new Date().getTime(),
+	async create(dto: CreateUserDto) {
+		const newUser = new UserEntity();
+		newUser.login = dto.login;
+		newUser.password = dto.password;
+		newUser.version = 1;
+		await this.userRepository.save(newUser);
+		return {
+			...newUser,
+			createdAt: new Date(newUser.createdAt).getTime(),
+			updatedAt: new Date(newUser.updatedAt).getTime(),
+			password: undefined,
 		};
-		database.users.push(newUser);
-		return { ...newUser, password: undefined };
 	}
 
 	async updatePassword(id: string, dto: UpdatePasswordDto): Promise<User> {
-		const user = database.users.find((user) => user.id === id);
+		const user = await this.userRepository.findOne({ where: { id } });
 		if (!user) {
 			throw new NotFoundException("User not found");
 		}
@@ -47,15 +54,20 @@ export class UserService {
 		}
 		user.password = dto.newPassword;
 		user.version += 1;
-		user.updatedAt = new Date().getTime();
-		return { ...user, password: undefined };
+		await this.userRepository.save(user);
+		return {
+			...user,
+			createdAt: new Date(user.createdAt).getTime(),
+			updatedAt: new Date(user.updatedAt).getTime(),
+			password: undefined,
+		};
 	}
 
 	async delete(id: string) {
-		const index = database.users.findIndex((user) => user.id === id);
-		if (!~index) {
+		const user = await this.userRepository.findOne({ where: { id } });
+		if (!user) {
 			throw new NotFoundException("User not found");
 		}
-		database.users.splice(index, 1);
+		await this.userRepository.delete(id);
 	}
 }
