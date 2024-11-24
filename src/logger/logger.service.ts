299550c9976dcd945ee37640abf0e2bcadd42641
Alexandr Chazov <chazovby@gmail.com>
2024-11-24 13:02:36 +0300

    feat: write logs into a file

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,4 +1,7 @@
 import { Injectable, LoggerService } from "@nestjs/common";
+import { constants } from "node:fs";
+import { access, mkdir, writeFile } from "node:fs/promises";
+import { join } from "node:path";
 
 import { LogLevel } from "../types";
 
@@ -14,42 +17,62 @@ const defaultLogLevels = `${LogLevel.log},${LogLevel.error},${LogLevel.warn},${L
 @Injectable()
 export class LoggerServiceCustom implements LoggerService {
 	logLevels: string[] = (process.env.LOG_LEVELS || defaultLogLevels).split(",");
+	logsFolderPath: string = join(__dirname, "..", "..", "logs");
 
 	log(message: any, ...optionalParams: any[]) {
 		if (this.logLevels.includes(LogLevel.log)) {
-			this.writeLog("LOG", message, green);
+			this.writeLog(LogLevel.log, message);
+			this.printLog(LogLevel.log, message, green);
 		}
 	}
 
 	error(message: any, ...optionalParams: any[]) {
 		if (this.logLevels.includes(LogLevel.error)) {
-			this.writeLog("ERROR", message, red);
+			this.writeLog(LogLevel.error, message);
+			this.printLog(LogLevel.error, message, red);
 		}
 	}
 
 	warn(message: any, ...optionalParams: any[]) {
 		if (this.logLevels.includes(LogLevel.warn)) {
-			this.writeLog("WARN", message, brown);
+			this.writeLog(LogLevel.warn, message);
+			this.printLog(LogLevel.warn, message, brown);
 		}
 	}
 
 	debug?(message: any, ...optionalParams: any[]) {
 		if (this.logLevels.includes(LogLevel.debug)) {
-			this.writeLog("DEBUG", message, blue);
+			this.writeLog(LogLevel.debug, message);
+			this.printLog(LogLevel.debug, message, blue);
 		}
 	}
 
 	verbose?(message: any, ...optionalParams: any[]) {
 		if (this.logLevels.includes(LogLevel.verbose)) {
-			this.writeLog("VERBOSE", message, yellow);
+			this.writeLog(LogLevel.verbose, message);
+			this.printLog(LogLevel.verbose, message, yellow);
 		}
 	}
 
-	private writeLog(level: string, message: string, color: string) {
+	private async writeLog(level: LogLevel, message: string): Promise<void> {
+		try {
+			await access(this.logsFolderPath, constants.F_OK);
+		} catch {
+			await mkdir(this.logsFolderPath, { recursive: true });
+		}
+		await writeFile(
+			join(this.logsFolderPath, fileName(level)),
+			`${getDate()} ${message}\n`,
+			{
+				encoding: "utf-8",
+				flag: "a",
+			},
+		);
+	}
+
+	private printLog(level: LogLevel, message: string, color: string): void {
 		const logMessage = `${getDate()} ${color} [${level}] ${resetColor} ${message}`;
-		console.log(logMessage);
-		// fs.appendFileSync(this.logFile, logMessage);
-		// this.checkLogRotation();
+		process.stdout.write(`${logMessage}\n`);
 	}
 }
 
@@ -61,6 +84,13 @@ function getDate() {
 	const minute = addZero(new Date().getMinutes());
 	const second = addZero(new Date().getSeconds());
 	return `${day}/${month}/${year}, ${hour}:${minute}:${second}`;
+}
+
+function fileName(level: LogLevel): string {
+	const year = new Date().getFullYear();
+	const month = addZero(new Date().getMonth() + 1);
+	const day = addZero(new Date().getDate());
+	return `${level}.${year}-${month}-${day}.log`;
 }
 
 function addZero(num: number): string {
